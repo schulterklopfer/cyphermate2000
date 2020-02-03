7222a39b210c8ad2ba523df477045f872f08420f
SKP <skp@skp.rocks>
2020-02-03 23:38:10 +0100

    added basic painting to memory wrapper methods

--- main.go
+++ main.go
@@ -11,16 +11,39 @@ func main() {
 
   println("e-Paper Init and Clear...")
   waveshare.EPD_4IN2_Init()
-  waveshare.EPD_4IN2_Clear()
+  //waveshare.EPD_4IN2_Clear()
+  //waveshare.DEV_Delay_ms(500)
+
+  blackImage, err := waveshare.CreateImage( waveshare.EPD_4IN2_WIDTH, waveshare.EPD_4IN2_HEIGHT )
+  defer waveshare.FreeImage( blackImage )
+
+  if err != nil {
+    println( "Error: "+err.Error() )
+    return
+  }
+
+  waveshare.Paint_NewImage( blackImage, waveshare.ROTATE_0, waveshare.BLACK )
+  waveshare.Paint_SelectImage( blackImage )
+
+
+  waveshare.Paint_Clear( waveshare.BLACK )
+  waveshare.EPD_4IN2_Display( blackImage )
+
   waveshare.DEV_Delay_ms(500)
 
-  //imagesize := waveshare.EPD_4IN2_WIDTH * waveshare.EPD_4IN2_HEIGHT
-
-  //blackImage := make( []byte, imagesize )
-
-  //waveshare.Paint_NewImage( blackImage )
   waveshare.Paint_Clear( waveshare.WHITE )
-  waveshare.Paint_Clear( waveshare.BLACK )
+  waveshare.EPD_4IN2_Display( blackImage )
+
+  waveshare.DEV_Delay_ms(500)
+
+  waveshare.Paint_ClearWindows( 30,30,100,100, waveshare.BLACK )
+  waveshare.EPD_4IN2_PartialDisplay( 30,30,100,100, blackImage )
+
+  waveshare.DEV_Delay_ms(500)
+
+  waveshare.Paint_ClearWindows( 30,30,100,100, waveshare.WHITE )
+  waveshare.EPD_4IN2_PartialDisplay( 30,30,100,100, blackImage )
+
   //waveshare.EPD_4IN2_Clear()
 
   waveshare.DEV_Module_Exit()
--- waveshare/waveshare.go
+++ waveshare/waveshare.go
@@ -10,7 +10,9 @@ package waveshare
 import "C"
 
 import (
-//"fmt"
+  //"fmt"
+  "errors"
+  "fmt"
   "unsafe"
 )
 
@@ -18,7 +20,10 @@ const EPD_4IN2_WIDTH  = C.EPD_4IN2_WIDTH
 const EPD_4IN2_HEIGHT = C.EPD_4IN2_HEIGHT
 const WHITE           = C.WHITE
 const BLACK           = C.BLACK
-
+const ROTATE_0        = C.ROTATE_0
+const ROTATE_90       = C.ROTATE_90
+const ROTATE_180      = C.ROTATE_180
+const ROTATE_270      = C.ROTATE_270
 
 func DEV_Module_Init() int {
   result := C.DEV_Module_Init()
@@ -33,18 +38,58 @@ func EPD_4IN2_Clear() {
   C.EPD_4IN2_Clear()
 }
 
+func EPD_4IN2_Display( imagePtr unsafe.Pointer ) {
+  C.EPD_4IN2_Display( (*C.uchar)(imagePtr) )
+}
+
+func EPD_4IN2_PartialDisplay( X_start uint16, Y_start uint16, X_end uint16, Y_end uint16, imagePtr unsafe.Pointer ) {
+  C.EPD_4IN2_PartialDisplay( C.ushort(X_start), C.ushort(Y_start), C.ushort(X_end), C.ushort(Y_end), (*C.uchar)(imagePtr))
+}
+
 func DEV_Delay_ms( delay int ) {
   C.DEV_Delay_ms( C.uint(delay) )
 }
 
-func Paint_NewImage( data []byte ) {
-  C.Paint_NewImage((*C.uchar)(unsafe.Pointer(&data[0])), EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 0, WHITE)
+func Paint_NewImage( imagePtr unsafe.Pointer, rotate C.ushort, color C.ushort ) {
+  C.Paint_NewImage( (*C.uchar)(imagePtr), EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, rotate, color)
 }
 
 func Paint_Clear( color C.ushort ) {
   C.Paint_Clear( color )
 }
 
+func Paint_ClearWindows( x1 uint16, y1 uint16, x2 uint16, y2 uint16, color C.ushort ) {
+  C.Paint_ClearWindows(C.ushort(x1), C.ushort(y1), C.ushort(x2), C.ushort(y2), color)
+}
+
+func Paint_SelectImage( imagePtr unsafe.Pointer ) {
+  C.Paint_SelectImage( (*C.uchar)(imagePtr) )
+}
+
 func DEV_Module_Exit() {
   C.DEV_Module_Exit()
+}
+
+func CreateImage( width uint16, height uint16 ) (unsafe.Pointer, error) {
+  /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+
+  imagesize := height
+  if width % 8 == 0 {
+    imagesize*=width / 8
+  } else {
+    imagesize*=width / 8 + 1
+  }
+  imagePtr := unsafe.Pointer(C.malloc(C.uint(imagesize)))
+
+  fmt.Printf("Created imagePtr %d\n", imagePtr )
+
+  if imagePtr == nil {
+    return nil, errors.New("Failed to apply for black memory");
+  }
+  return imagePtr,nil
+}
+
+func FreeImage( imagePtr unsafe.Pointer ) {
+  fmt.Printf("Freeing imagePtr %d\n", imagePtr )
+  C.free( imagePtr )
 }
